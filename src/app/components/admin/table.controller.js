53b4ebdd1c1ca4a576cbe8ba2a406cd1652934b0
siyuan <siyuan@siyuan.faevatech.com>
2016-03-30 16:42:30 +0800

    update ng-table

--- src/app/components/admin/table.controller.js
+++ src/app/components/admin/table.controller.js
@@ -34,7 +34,15 @@ function TableController($scope,NgTableParams,$filter) {
     {"id":4,"first_name":"Gloria","last_name":"Greene","email":"ggreene3@blogs.com","country":"Indonesia","ip_address":"69.115.85.157"},    {"id":3,"first_name":"Julie","last_name":"Wells","email":"jwells2@illinois.edu","country":"Finland","ip_address":"9.100.80.145"},
     {"id":4,"first_name":"Gloria","last_name":"Greene","email":"ggreene3@blogs.com","country":"Indonesia","ip_address":"69.115.85.157"},
     {"id":50,"first_name":"Andrea","last_name":"Greene","email":"agreene4@fda.gov","country":"Russia","ip_address":"128.72.13.52"}];
-
+  $scope.cols = [
+      { field: "id", title: "id", show: true },
+      { field: "first_name", title: "first_name", show: true },
+      { field: "last_name", title: "last_name", show: true },
+      { field: "email", title: "email", show: true },
+      { field: "country", title: "country", show: true },
+      { field: "ip_address", title: "ip_address", show: true },
+      { field: "action",title: "",dataType: "command"}
+   ];
   $scope.usersTable = new NgTableParams({
     page: 1,
     count: 10,
@@ -48,6 +56,40 @@ function TableController($scope,NgTableParams,$filter) {
       $defer.resolve($scope.data);
     }
   });
+  function cancel(row, rowForm) {
+        var originalRow = resetRow(row, rowForm);
+        angular.extend(row, originalRow);
+      }
+
+      function del(row) {
+      console.log($scope.usersTable.settings().dataset);
+        _.remove($scope.usersTable.settings().dataset, function(item) {
+          return row === item;
+        });
+        self.tableParams.reload().then(function(data) {
+          if (data.length === 0 && self.tableParams.total() > 0) {
+            self.tableParams.page(self.tableParams.page() - 1);
+            self.tableParams.reload();
+          }
+        });
+      }
+
+      function resetRow(row, rowForm){
+        row.isEditing = false;
+        rowForm.$setPristine();
+        self.tableTracker.untrack(row);
+        return _.findWhere(originalData, function(r){
+          return r.id === row.id;
+        });
+      }
+
+      function save(row, rowForm) {
+        var originalRow = resetRow(row, rowForm);
+        angular.extend(originalRow, row);
+      }
+    $scope.cancel = cancel;
+    $scope.del = del;
+    $scope.save = save;
 }
 angular
   .module('cupid')
